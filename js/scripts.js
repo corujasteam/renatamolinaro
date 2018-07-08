b83a6a40a3f509e279da364bb3b2a8b3dd88c06f
Luis Ferreira <luisrbferreira@gmail.com>
2018-07-08 14:25:57 -0300

    Ajustes

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    // $.mask.definitions['~'] = "[+-]";
+    // $("#celular").mask("(99) 99999-9999");
+    
+    // setCursor()
+    // function setCursor() {
+    //     $('#celular').setCursorPosition(1);
+    // }
+
+    $('#carousel').carousel(
+        {
+            fullWidth: true,
+        }
+    );
+
+    setInterval(function() {
+ 
+        $('#carousel').carousel('next');
+    
+    }, 1000);
+
+    $('#carousel2').carousel(
+        {
+            fullWidth: true,
+        }
+    );
+
+    setInterval(function() {
+ 
+        $('#carousel2').carousel('next');
+    
+    }, 4000);
+});
